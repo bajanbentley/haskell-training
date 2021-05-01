dec76606cdb9265adb38f106a31734742cbe0167
Kyle Bentley <kylebentley@gmx.com>
2021-05-01 22:57:06 +0000

    added some problems with my own solutions

--- starting_out/baby.hs
+++ starting_out/baby.hs
@@ -23,3 +23,16 @@ length' xs = sum [1 | _ <- xs]
 
 -- removeNonUppercase
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+
+
+-- numbers from 10 to 20 where x is not 13, 15 or 19
+-- [x | x <- [10..20], x /= 13, x /=15, x /=19]
+-- [x | x <- [10..20], not (x `elem` [13,15,19])]
+
+
+
+-- let rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+
+-- let rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+-- let rightTriangles' = [ (a,b,c) | (a,b,c) <- rightTriangles, a+b+c == 24]
