ca528b105b1dee6b9ef07198a97fb6c846b408d1
Kyle Bentley <kylebentley@gmx.com>
2021-05-01 22:54:29 +0000

    first class complete

--- starting_out/baby.hs
+++ starting_out/baby.hs
@@ -14,3 +14,12 @@ doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
 
 -- conanO'Brien
 conanO'Brien = "It's a me, Conan O'Brien!"
+
+-- boomBangs
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+-- length'
+length' xs = sum [1 | _ <- xs]
+
+-- removeNonUppercase
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
