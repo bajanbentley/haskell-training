efd24e6821c2276810b20de378034de4bfb3c3c6
Kyle Bentley <kylebentley@gmx.com>
2021-05-01 23:00:53 +0000

    made the comments runnable

--- starting_out/baby.hs
+++ starting_out/baby.hs
@@ -24,15 +24,13 @@ length' xs = sum [1 | _ <- xs]
 -- removeNonUppercase
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
-
-
 -- numbers from 10 to 20 where x is not 13, 15 or 19
--- [x | x <- [10..20], x /= 13, x /=15, x /=19]
--- [x | x <- [10..20], not (x `elem` [13,15,19])]
+notEqualTo = [x | x <- [10..20], x /= 13, x /=15, x /=19]
+notEqualTo' = [x | x <- [10..20], not (x `elem` [13,15,19])]
 
+-- a list of right angle triangles
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
 
-
--- let rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
-
--- let rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
--- let rightTriangles' = [ (a,b,c) | (a,b,c) <- rightTriangles, a+b+c == 24]
+-- a list of of right angle triangles whose perimiter does not exceed 24
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+rightTriangles'' = [ (a,b,c) | (a,b,c) <- rightTriangles, a+b+c == 24]
